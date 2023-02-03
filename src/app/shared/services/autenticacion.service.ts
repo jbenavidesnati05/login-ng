b27da286069f0b2128922dba7c93c4736e148382
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-02-03 11:03:56 -0500

    "login terminado"

---
+++ src/app/shared/services/autenticacion.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+
+    private ingresar:boolean= false;
+
+    constructor() { }
+
+    public ingresarAplicativo(obj:any):boolean{
+      this.ingresar=  obj.usuario =="jorge" && obj.password == "123";
+      return this.ingresar;
+    }
+
+
+    public habilitarlogeo(){
+      return this.ingresar;
+
+    }
+
+
+  }
+
